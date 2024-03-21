d25e8188f695270049279677c0a1c9acf9420ad9
DeveloperSenior <andresfescobar92@gmail.com>
2024-03-21 11:21:58 -0500

    RMV-back-node folder

--- back-node/security/auth.js
+++
@@ -1,9 +0,0 @@
-const {Response} = require('express')
-
-module.exports.isAuthorized  = function(req, res = Response, next) {
-
-    /*var err = new Error('Not authorized! Go back!');
-    err.status = 401;
-    res.status = 401;*/
-    return next();
-}
